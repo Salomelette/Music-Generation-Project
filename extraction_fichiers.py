fe2ce695f4d794c6f9fceec071034f7e839cdbcd
salomelette <salomeattiach@hotmail.fr>
2019-02-03 02:37:49 +0100

    extraction_fichiers corrigé

--- extraction_fichiers.py
+++ extraction_fichiers.py
@@ -3,7 +3,6 @@ import mido
 import os 
 
 database = os.listdir("/home/salom/ProjetM1/database")
-print(database[-1])
 
 def convert_midibd(database):
     bd = []
@@ -12,7 +11,7 @@ def convert_midibd(database):
             bd.append(mido.MidiFile("./database/"+str(m)))
         except IOError:
             print('MThd not found. Probably not a MIDI file')   
-            print("./database/"+str(m))
+            #print("./database/"+str(m))
 
     return bd 
         
@@ -21,14 +20,16 @@ def extract(database):
     res = []
     midibd = convert_midibd(database)
     for m in midibd:
-        for i, track in enumerate(m.tracks):
+        for track in m.tracks:
 #            print('Track {}: {}'.format(i, track.name))
             for msg in track:
-                midibd.append(msg.note%12)
+                if not msg.is_meta and msg.type == 'note_on':
+                    res.append(msg.note%12)
+                    
     return res 
 
 
 notes = extract(database)
-print(notes)
+print(len(notes))
         
     
