244a0dcf809e2db2fd7585821254a69ba21e5cfa
salomelette <salomeattiach@hotmail.fr>
2019-02-04 14:59:41 +0100

    extraction + doublets

--- extraction_fichiers.py
+++ extraction_fichiers.py
@@ -10,25 +10,47 @@ def convert_midibd(database):
         try:
             bd.append(mido.MidiFile("./database/"+str(m)))
         except IOError:
-            print('MThd not found. Probably not a MIDI file')   
-            #print("./database/"+str(m))
+            print('{} MThd not found. Probably not a MIDI file'.format(m))
 
     return bd 
-        
+
 
 def extract(database):
     res = []
     midibd = convert_midibd(database)
+    velocity = dict()
+    time = dict()
+    nb_occ = dict()
+    
+    for i in range(12):
+        velocity[i] = 0
+        time[i] = 0
+        nb_occ[i] = 0
+        
+    b = True 
+        
     for m in midibd:
         resm = []
         for track in m.tracks:
-#            print('Track {}: {}'.format(i, track.name))
+            if b :
+                print('Track {}: {}'.format(i, track.name))
             for msg in track:
                 if not msg.is_meta and msg.type == 'note_on':
-                    resm.append(msg.note%12)
+                    print(msg)
+                    note = msg.note%12 
+                    resm.append(note)
+                    velocity[note] += msg.velocity 
+                    time[note] += msg.time
+                    nb_occ[note] += 1 
+            b = False 
+
         res.append(resm)
-                    
-    return res 
+    
+    for i in range(12):
+        velocity[i] /= nb_occ[i]
+        time[i] /= nb_occ[i]
+                   
+    return res, velocity, time, nb_occ
 
 def find_doublet(list_track,nb):
     count=dict()
@@ -41,8 +63,9 @@ def find_doublet(list_track,nb):
     res=sorted(count.items(), key=lambda x: x[1],reverse=True)
     return res[:nb]
 
-notes = extract(database)
-print(len(notes))
-res=find_doublet(notes,15)
-print(res)
+notes, vel, time, nb_occ = extract(database)
+#print(len(notes), vel, time, nb_occ)
+res=find_doublet(notes,20)
+#print(res)
+
     
