4bbe7bfd8c17291da363d9617dd514d88f3af6b8
salomelette <salomeattiach@hotmail.fr>
2019-04-17 17:56:35 +0200

    test

--- datatools.py
+++ datatools.py
@@ -7,8 +7,8 @@ import pickle as pkl
 def prepareData(path,sequence_length=10):
 
     database = os.listdir(path)
-    notes, vel, nb_occ= extract(database)
-
+    notes, vel, nb_occ = extract(database)
+        
     for track in notes:
         track.insert(0,"BOT")
         track.append("EOT")
@@ -25,11 +25,50 @@ def prepareData(path,sequence_length=10):
                 sequence_in = track[i:i + sequence_length]
                 sequence_out = track[i+1:i + sequence_length+1]
                 yield sequence_in,sequence_out
-#    gen=generator()
-#    for i in gen:
-#        print(i)
+
     dataset = tf.data.Dataset.from_generator(generator,(tf.int64,tf.int64))
 
     with open("model_data.p",'wb') as file:
         pkl.dump({"occ":nb_occ,"n2i":notes2int,"i2n":int2notes,"vel":vel},file)
+
     return dataset,nb_occ,notes
+
+def prepareData_test(path,sequence_length=10,test=0.2):
+
+    database = os.listdir(path)
+    notes, vel, nb_occ, notes_test= extract(database)
+        
+    for track in notes+notes_test:
+        track.insert(0,"BOT")
+        track.append("EOT")
+        nb_occ["BOT"]+=1
+        nb_occ["EOT"]+=1
+
+    notes2int={u:i for i,u in enumerate(nb_occ.keys())}
+    int2notes = np.array(list(nb_occ.keys()))
+    notes_as_int = [np.array([notes2int[c] for c in track]) for track in notes]
+
+    notes_as_int_test = [np.array([notes2int[c] for c in track]) for track in notes+notes_test]
+
+    def generator():
+        for track in notes_as_int:
+            for i in range(0, len((track)) - sequence_length, 1):
+                sequence_in = track[i:i + sequence_length]
+                sequence_out = track[i+1:i + sequence_length+1]
+                yield sequence_in,sequence_out
+
+    dataset = tf.data.Dataset.from_generator(generator,(tf.int64,tf.int64))
+    
+    def generator_test():
+        for track in notes_as_int_test:
+            for i in range(0, len((track)) - sequence_length, 1):
+                sequence_in = track[i:i + sequence_length]
+                sequence_out = track[i+1:i + sequence_length+1]
+                yield sequence_in,sequence_out
+
+    dataset_test = tf.data.Dataset.from_generator(generator_test,(tf.int64,tf.int64))
+
+    with open("model_data.p",'wb') as file:
+        pkl.dump({"occ":nb_occ,"n2i":notes2int,"i2n":int2notes,"vel":vel},file)
+
+    return dataset,nb_occ,notes,dataset_test
--- make_train.py
+++ make_train.py
@@ -4,7 +4,7 @@ import numpy as np
 import os
 from datatools import prepareData
 import pickle as pkl
-
+import pdb
 
   
 def build_model(vocab_size, embedding_dim=256, rnn_units=1024, batch_size=64):
@@ -28,6 +28,23 @@ def loss(labels, logits):
 
 def train_model(datapath,sequence_length,batch_size,nb_epoch=100):
     dataset,nb_occ,notes=prepareData(datapath,sequence_length)
+    vocab_size=len(nb_occ)
+    examples_per_epoch = sum([len(track) for track in notes])//sequence_length
+    steps_per_epoch = examples_per_epoch//batch_size
+    
+    buffer_size=10000
+    dataset = dataset.shuffle(buffer_size).batch(batch_size, drop_remainder=True)
+    model = build_model(vocab_size = vocab_size,batch_size=batch_size)
+    model.compile(optimizer = tf.train.AdamOptimizer(),loss = loss)
+    checkpoint_dir = './training_checkpoints'
+    checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
+    checkpoint_callback=tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_prefix,save_weights_only=True,period=5)
+    pdb.set_trace()
+    
+    history = model.fit(dataset.repeat(), epochs=nb_epoch, steps_per_epoch=steps_per_epoch, callbacks=[checkpoint_callback])
+
+def train_model_test(datapath,sequence_length,batch_size,nb_epoch=100):
+    dataset,nb_occ,notes,dataset_test=prepareData(datapath,sequence_length)
     vocab_size=len(nb_occ)
     examples_per_epoch = sum([len(track) for track in notes])//sequence_length
     steps_per_epoch = examples_per_epoch//batch_size
